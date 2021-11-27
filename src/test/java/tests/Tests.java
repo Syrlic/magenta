12ebf8c5086f5a36e81789b33aa4f66c6fe5f45b
Syrlic <oklonina@gmail.com>
2021-11-27 06:21:25 +0400

    added fill form positive test contact us

--- src/test/java/tests/Tests.java
+++ src/test/java/tests/Tests.java
@@ -3,6 +3,7 @@ package tests;
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
@@ -52,18 +53,55 @@ public class Tests {
             $("input[name=email]").setValue("osipov@gmail.com").pressEnter();
         });
         step("Fill out phone number", () -> {
-            $("input[name=phone]").setValue("9023875883").pressEnter();
+            $("input[name=phone]").setValue("+7(495)557-29-29").pressEnter();
         });
         step("Checkbox selected", () -> {
             $("input#modalPrivacy").click();
         });
         step("Click on send request", () -> {
             $("button[type=button]").click();
-           //button disabled, false test
+            //button disabled, false test
         });
         step("Verify that request is sent", () -> {
             $("h5.modal-title").shouldBe(Condition.disappear);
         });
+
+    }
+
+    @Test
+    @DisplayName("Magenta tests")
+    void fillFormContactUsAtSectionTest() {
+
+        step("Open main page ", () -> {
+            open("https://magenta-technology.ru/");
+        });
+        step("Select section", () -> {
+            $$("div.dropdown").find(Condition.text("Решения")).hover()
+                    .$$("a").get(5).click();
+        });
+        step("Scroll to Send Request form", () -> {
+            $("section.mg-demo").scrollTo().click();
+        });
+        step("Fill out name", () -> {
+            $("input[name=name]").setValue("Валентин").pressEnter();
+        });
+        step("Fill out company name", () -> {
+            $("input[name=company]").setValue("AlfaBank").pressEnter();
+        });
+        step("Fill out email", () -> {
+            $("input[name=email]").setValue("vprohorov@gmail.com").pressEnter();
+        });
+        step("Fill out phone number", () -> {
+            $("input[name=phone]").setValue("+7(495)557-81-29").pressEnter();
+        });
+        step("Click on send request", () -> {
+            $("button[type=submit]").click();
+        });
+        step("Verify that request is sent", () -> {
+            $("div.mg-demo__thanks-content").shouldHave(Condition.text("Спасибо за заявку!"));
+        });
+
+
 
     }
 }
